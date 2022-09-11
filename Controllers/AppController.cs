fc78c7d61cbcc4794cd9566dace9b3728b947d80
Evan Frazier <e.frazier@nextech.com>
2022-09-11 23:03:39 +0100

    Added MVC implementation

---
+++ Controllers/AppController.cs
@@ -0,0 +1,53 @@
+﻿using DnDPuzzles.Services;
+using DnDPuzzles.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDPuzzles.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService mailService;
+
+        public AppController(IMailService mailService)
+        {
+            this.mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // send email
+                mailService.SendMessage("e.frazier@nextech.com", model.Subject, model.Message);
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+    }
+}
---
+++ Services/IMailService.cs
@@ -0,0 +1,7 @@
+﻿namespace DnDPuzzles.Services
+{
+    public interface IMailService
+    {
+        void SendMessage(string to, string subject, string body);
+    }
+}
